5180d1dffdcc33e54b29510ea178be83b0fe5ac0
rruya <harutojeu@gmail.com>
2024-02-26 16:10:09 +0700

    haloo

--- app/src/main/java/com/example/aplikasikotlin/tugas.kt
+++ app/src/main/java/com/example/aplikasikotlin/tugas.kt
@@ -1,2 +1,18 @@
 package com.example.aplikasikotlin
+fun main(args: Array<String>) {
+    biodata("Diaz Azkha Varissa", 19)
+    hobby("membaca", "membaca au")
+}
 
+fun biodata(name:String, age:Int){
+    // minimal 2 parameter/args
+    println("===============================")
+    println("Nama\t: $name")
+    println("Usia\t: $age")
+    println("===============================")
+}
+
+fun hobby(hobby:String, desc:String){
+    println("Saya suka $hobby")
+    println("Yang saya baca adalah $desc")
+}
